2df64ba81cf831e14deb495e82989db663dd928e
AlexVitko <vpa24082012@tut.by>
2023-02-13 22:01:20 +0300

    .\Example003-2_Random

--- Example003-2_Random/Program.cs
+++ Example003-2_Random/Program.cs
@@ -1,7 +1,6 @@
 ﻿int numberA = new Random().Next(1,10); //1 2 3 4...9
 Console.WriteLine(numberA);
 int numberВ = new Random().Next(1,10);
-Console.Write(numberВ); 
+Console.WriteLine(numberВ); 
 int result=numberA+numberВ;
 Console.WriteLine(result);
-
